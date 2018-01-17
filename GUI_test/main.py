fc30ceba139d8d6f0c85ff421d7987fe3e466c8c
hsm-hx <skytea922@gmail.com>
2018-01-17 12:42:56 +0900

    画面に画像を表示

--- GUI_test/main.py
+++ GUI_test/main.py
@@ -5,17 +5,26 @@ import sys
 
 def main():
   pygame.init()
-  screen = pygame.display.set_mode((1280, 720))
+  screen = pygame.display.set_mode((1280, 720), 0, 32)
   pygame.display.set_caption("リリアと虹色の夜想曲")
+  bg = pygame.image.load("img/bg.jpg").convert_alpha()
+  rect_bg = bg.get_rect()
 
   while(1):
-    screen.fill((0,0,0))
     pygame.display.update()
+    pygame.time.wait(30)
+    screen.fill((0,20,0, 0))
+    screen.blit(bg, rect_bg)
 
     for event in pygame.event.get():
       if event.type == QUIT:
         pygame.quit()
         sys.exit()
+      if event.type == KEYDOWN:
+        if event.key == K_ESCAPE:
+          pygame.quit()
+          sys.exit()
+
 if __name__ == "__main__":
   main()
   
