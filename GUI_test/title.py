17838f459a5f8452c3645e82e7dff4179e736aef
hsm-hx <skytea922@gmail.com>
2018-01-18 22:24:59 +0900

    title.pyにメニュー表示を追加

--- GUI_test/title.py
+++ GUI_test/title.py
@@ -9,12 +9,18 @@ def main():
   pygame.display.set_caption("リリアと虹色の夜想曲")
   bg = pygame.image.load("img/bg.jpg").convert_alpha()
   rect_bg = bg.get_rect()
+  font = pygame.font.Font(None, 55)
 
   while(1):
-    pygame.display.update()
-    pygame.time.wait(30)
+    menu_start = font.render("START", True, (255, 255, 255))
+    menu_quit = font.render("QUIT", True, (255, 255, 255))
+
     screen.fill((0,20,0, 0))
     screen.blit(bg, rect_bg)
+    screen.blit(menu_start, [100, 200])
+    screen.blit(menu_quit, [100, 100])
+
+    pygame.display.update()
 
     for event in pygame.event.get():
       if event.type == QUIT:
