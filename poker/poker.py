218268fed12e0d9aa93ea2ba2357c8b0d655689d
hsm-hx <skytea922@gmail.com>
2018-01-19 10:27:43 +0900

    show_text.pyをpoker.pyにリネーム

--- poker/poker.py
+++ poker/poker.py
@@ -7,16 +7,27 @@ import ability
 SUIT = 4
 RANK = 13
 
+(w, h) = (1280, 720)
+pygame.init()
+pygame.display.set_mode((w, h), 0, 32)
+screen = pygame.display.get_surface()
+
 class Card():
   def __init__(self, suit, rank):
-    if suit==0: self.suit="Heart"
-    elif suit==1: self.suit="Dia"
-    elif suit==2: self.suit="Spade"
-    else: self.suit="Club"
+    if suit==0: self.suit="heart"
+    elif suit==1: self.suit="diamond"
+    elif suit==2: self.suit="spade"
+    else: self.suit="club"
 
     self.rank=rank
 
-  def show(self):
+    if self.rank < 10:
+      img_path = "img/card_" + self.suit + "_0" + str(self.rank) + ".png"
+    else:
+      img_path = "img/card_" + self.suit + "_" + str(self.rank) + ".png"
+    self.img = pygame.image.load(img_path).convert_alpha()
+    
+  def show(self, pos_x, pos_y):
     print(self.suit+'-', end='')
 
     if self.rank == 1: print('A')
@@ -24,7 +35,8 @@ class Card():
     elif self.rank == 12: print('Q')
     elif self.rank == 13: print('K')
     else: print(self.rank)
-
+    
+    screen.blit(self.img, (pos_x, pos_y)) 
 
 def deck_init(deck):
   count = 0
@@ -35,7 +47,7 @@ def deck_init(deck):
       count += 1
 
 def hand_init(hand):
-  for i in hand: hand = Card(0,0)
+  for i in hand: hand = Card(0,1)
 
 def hand_draw(index, hand, deck):
   hand[index] = deck[random.randint(0, 52-1)]
@@ -49,15 +61,18 @@ def main():
   hand = 5*[0] # initialize by 0
   change = 5*[9]
   count = 0
+  
   deck_init(deck)
   hand_init(hand)
   for i in range(len(hand)):
     hand_draw(i, hand, deck)
+    pygame.display.update()
 
   for i in range(len(hand)):
     print(str(i+1) + " : ", end="")
-    hand[i].show()
+    hand[i].show(i*100, 500)
 
+  pygame.display.update() 
   #Change hands
   print("交換したいカードを選択してください。")
   print("Fキーで選択を終了")
@@ -86,10 +101,13 @@ def main():
   print("\nChange Result")
   for i in range(len(hand)):
     print(str(i+1) + " : ", end="")
-    hand[i].show()
-
+    hand[i].show(i*100, 500)
+ 
+  pygame.display.update() 
   #sort by rank
   hand = sorted(hand, key=attrgetter('rank'))
 
   print(judge.judgement(hand))
-  
+
+
+main()
